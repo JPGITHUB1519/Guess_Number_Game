a26a1fdf4208aec9162bc5e833cd5ba165d81bbb
Jean <juanpedro1519@gmail.com>
2016-04-18 13:41:24 -0400

    Analize number Fix return Values

--- guess_number.py
+++ guess_number.py
@@ -9,10 +9,21 @@ def analize_number(number, random_number) :
 	if(number < random_number) :
 
 		print "The Guess number is greater than your input"
+		return False
 
 	if(number > random_number) :
 
 		print "The Guess number is less than your input"
+		return False
+	return True
+	
 
 
-analize_number(5, generar_aleatorio(0,10))
+
+        
+    
+ 
+
+        
+
+
