055c81cd371e24424db24e5bf66d79dc4cd2605d
Jean <juanpedro1519@gmail.com>
2016-04-18 13:33:08 -0400

    ADD analize_number

--- guess_number.py
+++ guess_number.py
@@ -4,6 +4,15 @@ def generar_aleatorio(mini,maxi) :
 
 	return randint(mini,maxi)
 
+def analize_number(number, random_number) :
 
-print generar_aleatorio(0,10)
+	if(number < random_number) :
 
+		print "The Guess number is greater than your input"
+
+	if(number > random_number) :
+
+		print "The Guess number is less than your input"
+
+
+analize_number(5, generar_aleatorio(0,10))
